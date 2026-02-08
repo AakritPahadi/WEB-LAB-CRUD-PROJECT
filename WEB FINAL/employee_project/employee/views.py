193ee1ba758dbf8c497566e3e0270869fa7364fa
Aakrit Pahadi <180197474+AakritPahadi@users.noreply.github.com>
2026-02-08 16:15:37 +0545

    Add files via upload

---
+++ WEB FINAL/employee_project/employee/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import Employee
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employee/list.html', {'employees': employees})
+
+def add_employee(request):
+    if request.method == 'POST':
+        Employee.objects.create(
+            name=request.POST['name'],
+            code=request.POST['code'],
+            salary=request.POST['salary'],
+            city=request.POST['city']
+        )
+        return redirect('/')
+    return render(request, 'employee/add.html')
+
+def delete_employee(request, id):
+    Employee.objects.get(id=id).delete()
+    return redirect('/')
